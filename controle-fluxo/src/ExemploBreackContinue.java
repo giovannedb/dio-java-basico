f90864f5d28df4b1c1fb1e919932f40b623bd278
Giovanne Dassi <84192470+giovannedb@users.noreply.github.com>
2022-11-15 15:15:18 -0300

    estruturas de repetição

---
+++ controle-fluxo/src/ExemploBreackContinue.java
@@ -0,0 +1,13 @@
+public class ExemploBreackContinue {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <= 5; numero++){
+            if(numero == 3){
+                //break;
+                continue; // ele pula o numero 3
+            }
+            System.out.println(numero);
+        }
+    }
+    
+}
+ 
